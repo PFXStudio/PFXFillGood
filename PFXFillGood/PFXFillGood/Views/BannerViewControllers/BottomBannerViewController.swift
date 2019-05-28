329558ae59cfc89eef39ae330df966b52a1a9afb
PFXStudio <pfxstudio21@gmail.com>
2019-05-28 16:11:32 +0900

    update admob

---
+++ PFXFillGood/PFXFillGood/Views/BannerViewControllers/BottomBannerViewController.swift
@@ -0,0 +1,60 @@
+//
+//  BottomBannerViewController.swift
+//  PFXFillGood
+//
+//  Created by succorer on 28/05/2019.
+//  Copyright © 2019 PFXStudio. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class BottomBannerViewController: UIViewController {
+
+    var bannerView: GADBannerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        
+        addBannerViewToView(bannerView)
+        
+        bannerView.adUnitID = "ca-app-pub-6807878951364224/8418672303"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+    }
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: bottomLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PFXFillGood/PFXFillGood/Views/StartViewController.swift
+++ PFXFillGood/PFXFillGood/Views/StartViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import GoogleMobileAds
 import ProgressHUD
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, GADInterstitialDelegate {
 
+    var interstitial: GADInterstitial!
     var gameViewController: GameViewController?
+    var results = [CGPoint]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        interstitial = createAndLoadInterstitial()
     }
     
+    func createAndLoadInterstitial() -> GADInterstitial {
+        let interstitial = GADInterstitial(adUnitID: "ca-app-pub-6807878951364224/2401371946")
+        interstitial.delegate = self
+        interstitial.load(GADRequest())
+        return interstitial
+    }
+    
+    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+        interstitial = createAndLoadInterstitial()
+    }
+    
+    func interstitialWillDismissScreen(_ ad: GADInterstitial) {
+        TileData.shared.paths = self.results
+    }
+
+    func checkReady() {
+        DispatchQueue.global().asyncAfter(wallDeadline: .now() + 0.3) {
+            if self.interstitial.isReady == true {
+                self.interstitial.present(fromRootViewController: self)
+                DispatchQueue.main.async {
+                }
+                
+                return
+            }
+            else {
+                self.checkReady()
+            }
+        }
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         GameStatus.shared = StartStatus()
@@ -141,6 +175,28 @@ class StartViewController: UIViewController {
             return
         }
         
+        self.gameViewController?.gameScene?.clearUnit()
+        let tiles = TileData.shared.tiles
+        var paths = [CGPoint]()
+        paths.append(TileData.shared.startPoint)
+        ProgressHUD.show(NSLocalizedString("searchingPaths", comment: ""), interaction: false)
+        DispatchQueue.global().async {
+            let result = self.recursivePath(parentTiles: tiles, parentPaths: paths)
+            if result.0 == true {
+                ProgressHUD.showSuccess(NSLocalizedString("successPaths", comment: ""))
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                    self.interstitial.present(fromRootViewController: self)
+                    self.results = result.1
+                })
+            }
+            else {
+                // no paths
+                DispatchQueue.main.async {
+                    ProgressHUD.showError(NSLocalizedString("failPaths", comment: ""))
+                }
+            }
+        }
+        
         /*
         // check tile
         var blockTileCount = 0
@@ -185,27 +241,5 @@ class StartViewController: UIViewController {
             }
         }
         */
-        
-        self.gameViewController?.gameScene?.clearUnit()
-        let tiles = TileData.shared.tiles
-        var paths = [CGPoint]()
-        paths.append(TileData.shared.startPoint)
-        ProgressHUD.show(NSLocalizedString("searchingPaths", comment: ""), interaction: false)
-        DispatchQueue.global().async {
-            let result = self.recursivePath(parentTiles: tiles, parentPaths: paths)
-            if result.0 == true {
-                ProgressHUD.showSuccess(NSLocalizedString("successPaths", comment: ""))
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-                    // TODO : 광고
-                    TileData.shared.paths = result.1
-                })
-            }
-            else {
-                // no paths
-                DispatchQueue.main.async {
-                    ProgressHUD.showError(NSLocalizedString("failPaths", comment: ""))
-                }
-            }
-        }
     }
 }
